184669f15aa7e9e74240ff9b87cb4eb718a40ba9
Nico König <nico.koenig@coding-bootcamps.eu>
2022-12-14 12:18:33 +0100

    docs: day 1 of js intro

---
+++ live-coding/day-1/script.js
@@ -0,0 +1,117 @@
+// console.log("Hallo Welt!!!!");
+// console.log("1234");
+
+// Kommentar - Single Line Comment
+
+/* */ // Multi Line Comment
+
+// todo: mach das schöner
+
+/* 
+console.log("Hallo Tim!!!!");
+console.log("Hallo Tim!!!!");
+console.log("Hallo Tim!!!!");
+console.log("Hallo Tim!!!!");
+console.log("Hallo Tim!!!!");
+console.log ("Hallo Tim!!!!");
+*/
+// debugger;
+
+// var a; // Deklaration der Variable a
+// console.log(a);
+// a = 1; // Wertzuweisung
+// console.log(a);
+// a = 2; // Wertzuweisung
+// console.log(a);
+
+//const c;
+
+// Keywords für die Variablenerstellung
+// var
+// let
+// const
+
+// const c = 1;
+// c = 2; // Fehler!
+
+// var vs let?
+
+// let a = 1;
+// a = 2;
+// console.log(a);
+
+// const b = 1;
+// b = 2;
+
+// // Initialisierung einer Variable
+// let a; // Deklaration
+// a = 1; // initialisierung
+
+// // Deklaration und Initialisierung in einem
+// let b = 1;
+
+// Datentypen
+
+// Datentypen
+
+// let a = 1.1; // float = fließkommazahl
+// let b = 1; // integer = ganzzahlig
+
+// Boolean --> true oder false
+// let a = false; // 0
+// let b = true; // 1
+// console.log(a);
+// console.log(b);
+
+// let a = "123";
+// console.log(a);
+// a = 123;
+// console.log(a);
+
+// let a = 1;
+// a = false;
+// a = "asdfsadf";
+
+// undefined vs null
+//
+
+// let user;
+// console.log(user);
+// user = "Nico";
+// console.log(user);
+// user = null;
+// console.log(user);
+
+// console.log(1 + 2 + 3);
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let d = a + b;
+// d = d + c;
+
+// let d = a + b + c;
+
+// let a;
+// let c;
+// let b = (c = a = 1);
+// console.log(a);
+// console.log(b);
+
+//  Mathematische Operationen
+
+// console.log(1 + 2);
+// console.log(1 - 2);
+// console.log(1 * 2);
+// console.log(1 / 2);
+
+// Modulo = restwert operator --> remainder operator
+// console.log(10 % 3);
+// console.log(10 % 4);
+// console.log(10 % 5);
+
+// const a = 10;
+// const b = 3;
+// const rest = 10 % 3;
+// const bTimesInA = (a - rest) / b;
+// console.log(bTimesInA);
---
+++ tasks/day-1.md
@@ -0,0 +1,59 @@
+# Tasks Day 1
+
+## Task: Gettings Started with JavaScript
+
+### Step 1
+
+- [ ] Create a html document with file name "gettings-started-with-js.html" that loads an script file
+- [ ] The script should log the string "Hello World" to the console of the browser
+
+### Step 2
+
+- [ ] Deactivate the console.log from the previous task
+- [ ] Add a variable with the name myName to the script and assign your personal name as the value
+- [ ] Log your name using the variable to the console of the browser
+
+### Step 3
+
+- [ ] Create another varible with the name greeting and assign the value "Hallo"
+- [ ] Log the greeting and your name to the console of the browser
+
+## Task: Net and Gross Price
+
+```js
+const netPrice = 25;
+const tax = 19;
+
+// TODO: create a variable grossPrice
+// TODO: calculate the gross (Brutto) price of the net price
+```
+
+## Task: Spot the errors
+
+The following code exampels contain little mistakes - can you spot them?
+
+```js
+console.log("Hallo";
+```
+
+```js
+const a = 1;
+a = 2;
+```
+
+```js
+const 1 = "a";
+```
+
+```js
+const let = undefined;
+```
+
+```js
+return;
+console.log("I cant see the console log in the console?!");
+```
+
+```js
+console.log("That doesn"t work");
+```
